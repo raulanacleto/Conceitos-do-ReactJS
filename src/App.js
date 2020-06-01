6f83c115b193ced247d4b08581f695bf5ef69b7d
raulanacleto <raulanacletosilva@gmail.com>
2020-06-01 20:10:31 -0300

    commit-desafio

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React , {useState, useEffect} from 'react';
+
+import api from './services/api'
+
+import "./styles.css";
+
+function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+        console.log(response);
+        setRepositories(response.data)
+    })
+  },[]);
+
+
+  async function handleAddRepository() {
+
+    const response = await api.post('repositories',{
+      title: `titulo ${Date.now()}`,
+      url: "www.raul.com",
+      techs: ["um", "dois", "tres"]
+    })
+
+  const repository = response.data;
+
+  setRepositories([...repositories, repository])
+
+  }
+
+  async function handleRemoveRepository(id) {
+
+    await api.delete(`repositories/${id}`)
+     setRepositories(repositories.filter(rep => {
+       return rep.id !== id
+     }))
+     
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository =>
+        <li key={repository.id}>{repository.title}
+
+        <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>
+
+        )}
+
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
